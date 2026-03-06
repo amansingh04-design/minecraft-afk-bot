3c2dbe9c5763a1a74c760bf02a89f5e3e7af5b54
amansingh04-design <amansinghkv2010@gmail.com>
2026-03-06 09:59:59 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+const mineflayer = require('mineflayer')
+
+const bot = mineflayer.createBot({
+  host: 'YOUR_SERVER_IP',
+  port: 25565,
+  username: 'AFK_Bot'
+})
+
+bot.on('spawn', () => {
+  console.log("Bot joined!")
+
+  setInterval(() => {
+    bot.setControlState('jump', true)
+    setTimeout(() => bot.setControlState('jump', false), 500)
+  }, 15000)
+})
